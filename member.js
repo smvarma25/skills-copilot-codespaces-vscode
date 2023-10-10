11ba1f5e80b2e8f182071b30ae318f91925b0870
smvarma25 <34524329+smvarma25@users.noreply.github.com>
2023-10-10 21:05:29 +0000

    Copilot fourth commit

--- member.js
+++ member.js
@@ -1,3 +1,8 @@
 function skillsMember() {
-    // Your code here
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'memberCtrl'
+    };
 }
